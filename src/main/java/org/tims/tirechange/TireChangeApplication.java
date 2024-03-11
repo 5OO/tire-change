2527019aab9246338066cbd27ed75b3e8acc7226
5OO <git.hub@myrk.eu>
2024-03-11 18:08:58 +0200

    Initial commit

---
+++ src/main/java/org/tims/tirechange/TireChangeApplication.java
@@ -0,0 +1,13 @@
+package org.tims.tirechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireChangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireChangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tims/tirechange/TireChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tims.tirechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireChangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
