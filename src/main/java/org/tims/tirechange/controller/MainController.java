ffbbfdb9b235f45a0c6c1c736a8df71ee195e3de
5OO <git.hub@myrk.eu>
2024-03-12 23:34:27 +0200

    Add MainController class with TireShopApi list
    Created a new MainController class that keeps a list of TireShopApi instances. This controller paves the way for fetching tire shop data from multiple APIs, optimizing service integration. Additional methods for more complex functionalities like booking will be added in future updates.

---
+++ src/main/java/org/tims/tirechange/controller/MainController.java
@@ -0,0 +1,11 @@
+package org.tims.tirechange.controller;
+
+import org.tims.tirechange.api.TireShopApi;
+
+import java.util.List;
+
+public class MainController {
+    private List<TireShopApi> tireShopApis;
+
+
+}
