9dc54ef46eb628179ee817e13fc10726d1c8ac1f
5OO <git.hub@myrk.eu>
2024-03-12 23:40:42 +0200

    Add TireChangeTimesResponse model class
    Added a new model class named TireChangeTimesResponse. This class is annotated with Jackson properties for XML processing and equipped with getter and setter methods. It deals with the list of available times for tire changes.

---
+++ src/main/java/org/tims/tirechange/model/TireChangeTimesResponse.java
@@ -0,0 +1,18 @@
+package org.tims.tirechange.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JacksonXmlRootElement(localName = "tireChangeTimesResponse")
+public class TireChangeTimesResponse {
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "availableTime")
+    private List<TireChangeTime> availableTimes;
+}
