345db106490d3814ec873c55aac98ea3765cb0d0
5OO <git.hub@myrk.eu>
2024-03-12 23:41:37 +0200

    Add IOException to TireShopApi methods
    Updated the TireShopApi interface to include IOException in the method signature for getAvailableTimes. This change is required to handle scenarios where a potential IO operation fails during execution.

--- src/main/java/org/tims/tirechange/api/TireShopApi.java
+++ src/main/java/org/tims/tirechange/api/TireShopApi.java
@@ -2,12 +2,13 @@ package org.tims.tirechange.api;
 
 import org.tims.tirechange.model.TireChangeBooking;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TireShopApi {
     List<TireChangeBooking> getAvailableTimes(LocalDateTime from,
-                                              LocalDateTime until);
+                                              LocalDateTime until) throws IOException;
 
     TireChangeBooking bookTimeSlot(String universalId,
                                    String contactInformation);
