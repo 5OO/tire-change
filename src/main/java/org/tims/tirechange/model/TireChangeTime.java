3350dff3c7a0cbf06b26e741f93c9758e7630e77
5OO <git.hub@myrk.eu>
2024-03-12 23:39:54 +0200

    Add TireChangeTime model class
    Added a new model class named TireChangeTime. This class is annotated with Jackson properties for XML processing and is equipped with getter and setter methods for two fields, 'uuid' and 'time'.

---
+++ src/main/java/org/tims/tirechange/model/TireChangeTime.java
@@ -0,0 +1,18 @@
+package org.tims.tirechange.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JacksonXmlRootElement(localName = "availableTime")
+public class TireChangeTime {
+
+    @JacksonXmlProperty(isAttribute = true)
+    private String uuid;
+
+    @JacksonXmlProperty
+    private String time;
+}
