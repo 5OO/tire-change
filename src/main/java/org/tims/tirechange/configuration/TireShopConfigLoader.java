674d9f97289b617d67281040f373010ad67dd23a
5OO <git.hub@myrk.eu>
2024-03-12 23:37:37 +0200

    Add new TireShopConfigLoader class
    This commit introduces the TireShopConfigLoader class, which helps in loading configuration details for tire shops. It parses specified configuration files using ObjectMapper and returns as a list of TireShopConfig objects. This functionality aids in dynamic configuration control for individual tire shops.

---
+++ src/main/java/org/tims/tirechange/configuration/TireShopConfigLoader.java
@@ -0,0 +1,18 @@
+package org.tims.tirechange.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class TireShopConfigLoader {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<TireShopConfig> loadConfig(String filePath)  throws IOException {
+        TireShopConfig[] configs = objectMapper.readValue(new File(filePath), TireShopConfig[].class);
+        return List.of(configs);
+    }
+}
