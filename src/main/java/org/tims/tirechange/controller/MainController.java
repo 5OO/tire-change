c72ae3a5c0cabd45000dd0b31caa5ced3aea4305
5OO <git.hub@myrk.eu>
2024-03-12 23:57:55 +0200

    Expand MainController with new API endpoint
    MainController class has been expanded with a new API endpoint. This API endpoint allows retrieving available times for tire change bookings from the LondonApi. This helps clients to check availability within a provided date range.

--- src/main/java/org/tims/tirechange/controller/MainController.java
+++ src/main/java/org/tims/tirechange/controller/MainController.java
@@ -1,11 +1,27 @@
 package org.tims.tirechange.controller;
 
-import org.tims.tirechange.api.TireShopApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.tims.tirechange.api.LondonApi;
+import org.tims.tirechange.model.TireChangeBooking;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
+@RestController
 public class MainController {
-    private List<TireShopApi> tireShopApis;
 
+    @Autowired
+    private LondonApi londonApi;
 
+    @GetMapping("/api/tire-changes/available")
+    public List<TireChangeBooking> getAvailableTimes(
+            @RequestParam LocalDateTime from,
+            @RequestParam LocalDateTime until) throws IOException {
+
+        return londonApi.getAvailableTimes(from, until);
+    }
 }
