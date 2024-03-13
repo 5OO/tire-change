29443e3891756ff1288cc5d2457bc6b554c6ac73
5OO <git.hub@myrk.eu>
2024-03-13 18:45:11 +0200

    Remove unused DateTimeFormat import in MainController
    Removed an unused import (DateTimeFormat) from MainController.java to clean up the code.

--- src/main/java/org/tims/tirechange/controller/MainController.java
+++ src/main/java/org/tims/tirechange/controller/MainController.java
@@ -1,7 +1,6 @@
 package org.tims.tirechange.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
