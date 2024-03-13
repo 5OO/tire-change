0fa08c3e442b19e8ec495e14fbd3d23f847804f1
5OO <git.hub@myrk.eu>
2024-03-13 16:56:31 +0200

    the endpoint URL for getting available times has been updated for more simplicity; input parameters are expected to be limited to date only, so discarded time and specified expected format.

--- src/main/java/org/tims/tirechange/controller/MainController.java
+++ src/main/java/org/tims/tirechange/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.tims.tirechange.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.tims.tirechange.api.LondonApi;
 import org.tims.tirechange.model.TireChangeBooking;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -17,11 +18,10 @@ public class MainController {
     @Autowired
     private LondonApi londonApi;
 
-    @GetMapping("/api/tire-changes/available")
+    @GetMapping("/tire-changes/available")
     public List<TireChangeBooking> getAvailableTimes(
-            @RequestParam LocalDateTime from,
-            @RequestParam LocalDateTime until) throws IOException {
-
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate from,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate until) throws IOException {
         return londonApi.getAvailableTimes(from, until);
     }
 }
