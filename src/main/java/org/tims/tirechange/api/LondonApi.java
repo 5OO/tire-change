fa81c0d3160edc49e446381ca0cdbe8ea648d58b
5OO <git.hub@myrk.eu>
2024-03-13 18:55:34 +0200

    Refactor and implement LondonApi booking logic
    The LondonApi getAvailableTimes method has been refactored to successfully parse XML data and convert it into a list of TireChangeBooking objects. The previous incomplete code, TODOs and comments have been replaced by a working implementation that successfully processes response data. This also involved removing an unused import.

--- src/main/java/org/tims/tirechange/api/LondonApi.java
+++ src/main/java/org/tims/tirechange/api/LondonApi.java
@@ -7,12 +7,13 @@ import org.springframework.web.client.RestTemplate;
 import org.tims.tirechange.configuration.TireShopConfig;
 import org.tims.tirechange.configuration.TireShopConfigLoader;
 import org.tims.tirechange.model.TireChangeBooking;
-import org.tims.tirechange.model.TireChangeTime;
 import org.tims.tirechange.model.TireChangeTimesResponse;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component // Mark this as a Spring-managed bean
 public class LondonApi implements TireShopApi {
@@ -24,7 +25,6 @@ public class LondonApi implements TireShopApi {
     private TireShopConfigLoader configLoader;
 
     public List<TireChangeBooking> getAvailableTimes(LocalDate from, LocalDate until) throws IOException {
-        // TODO: Implement logic to:
         // 1. Build the API request URL (use the configuration data)
         TireShopConfig config = configLoader.loadConfig("src/main/resources/tire_shops.json").get(0); // Load and retrieve config
         String endpoint = config.getApi().getEndpoint();
@@ -35,31 +35,27 @@ public class LondonApi implements TireShopApi {
         // 2. Make an HTTP GET request to the London API
         String xmlResponse = restTemplate.getForObject(url, String.class);
 
+        // Parse XML
         XmlMapper xmlMapper = new XmlMapper();
         TireChangeTimesResponse response = xmlMapper.readValue(xmlResponse, TireChangeTimesResponse.class);
 
-// Access available times:
-        List<TireChangeTime> availableTimes = response.getAvailableTimes();
-        for (TireChangeTime time : availableTimes) {
-            System.out.println("public class LondonApi implements TireShopApi is here with UUID: " + time.getUuid());
-            System.out.println("public class LondonApi implements TireShopApi is here with Time: " + time.getTime());
 
+        // Map to TireChangeBooking
+        List<TireChangeBooking> availableTimes = response.getAvailableTimes().stream()
+                .map(time -> {
+                    TireChangeBooking availableTime = new TireChangeBooking();
+                    availableTime.setUniversalId(time.getUuid());
+                    availableTime.setBookingTime(java.time.OffsetDateTime.parse(time.getTime()).toLocalDateTime());
+                    availableTime.setAvailable(true);
+                    availableTime.setVehicleType(Arrays.toString(config.getVehicleTypes()));
+                    availableTime.setTireShopName(config.getName());
+                    availableTime.setTireShopAddress(config.getAddress());
+                    return availableTime;
+                })
+                .collect(Collectors.toList());
 
-// TODO: Parse the XML response into TireChangeBooking objects
-
-// ... return the list of parsed bookings
-//        return bookings;
-            // 3. Parse the XML response
-            // 4. Convert the XML data into a list of TireChangeBooking objects
-
-        }
-        return null;
+        return availableTimes;
     }
-
-//    @Override
-//    public List<TireChangeBooking> getAvailableTimes(LocalDate from, LocalDate until) throws IOException {
-//        return null;
-//    }
 
     @Override
     public TireChangeBooking bookTimeSlot(String universalId, String contactInformation) {
