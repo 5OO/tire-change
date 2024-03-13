40fabbadce103208c8ca90aac7034fd37f28a363
5OO <git.hub@myrk.eu>
2024-03-13 18:52:50 +0200

    Replace RequiredArgsConstructor with Getter and Setter in TireChangeBooking
    Updated TireChangeBooking.java to swap @RequiredArgsConstructor with @Getter and @Setter to adhere to more specific Lombok practices.

--- src/main/java/org/tims/tirechange/model/TireChangeBooking.java
+++ src/main/java/org/tims/tirechange/model/TireChangeBooking.java
@@ -1,16 +1,19 @@
 package org.tims.tirechange.model;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-@RequiredArgsConstructor
+@Getter
+@Setter
 public class TireChangeBooking {
     private String universalId; // booking identifier for the  in London API 1 (UUID) or available tire change time (ID) in Manchester API 2
-    private LocalDateTime bookingTime; // selected booking Date and time of the tire change
-    private boolean available; // Indicates if the time slot is available for booking API 2 Manchester ws
-    private String vehicleType; // Type of vehicle that is serviced - need to display in UI
+    private LocalDateTime bookingTime; // booking Date and time of the tire change
+    private boolean available; // Indicates if the time slot is available for booking API 2 Manchester ws; NO => it is the booked slot
+    private String vehicleType; // Type of vehicle that is serviced                                         - need to display in UI
     private String apiIdentifier; // to indicate which API a specific booking is associated with
-    private String tireShopName; // Name of the tire workshop - need to display in UI
-    private String tireShopAddress; // Address of the tire workshop - need to display in UI
+    private String tireShopName; // Name of the tire workshop                                               - need to display in UI
+    private String tireShopAddress; // Address of the tire workshop                                         - need to display in UI
     private String clientContactInformation; // optional contact information of the client booking for the time slot (incl. within the Request body)
 }
