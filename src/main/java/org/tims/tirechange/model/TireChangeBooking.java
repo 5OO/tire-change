aeb4969afab20e8d4d91346189b6e37a93295291
5OO <git.hub@myrk.eu>
2024-03-13 17:20:13 +0200

    Revise TireChangeBooking model
    deleted obsolete fields as bookingTime already represents the specific time slots.

--- src/main/java/org/tims/tirechange/model/TireChangeBooking.java
+++ src/main/java/org/tims/tirechange/model/TireChangeBooking.java
@@ -5,14 +5,12 @@ import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 public class TireChangeBooking {
-    private String universalId; // booking identifier for the  in London API (UUID) or available tire change time ID in Manchester
+    private String universalId; // booking identifier for the  in London API 1 (UUID) or available tire change time (ID) in Manchester API 2
     private LocalDateTime bookingTime; // selected booking Date and time of the tire change
-    private boolean available; // Indicates if the time slot is available for booking
-    private String vehicleType; // Type of vehicle that is serviced
+    private boolean available; // Indicates if the time slot is available for booking API 2 Manchester ws
+    private String vehicleType; // Type of vehicle that is serviced - need to display in UI
     private String apiIdentifier; // to indicate which API a specific booking is associated with
-    private String tireShopName; // Name of the tire workshop
-    private String tireShopAddress; // Address of the tire workshop
-    private String clientContactInformation; // Contact information of the client booking for the time slot
-    private LocalDateTime dateFrom; // API1 London time slots from
-    private LocalDateTime dateUntil; // API1 London time slots until
+    private String tireShopName; // Name of the tire workshop - need to display in UI
+    private String tireShopAddress; // Address of the tire workshop - need to display in UI
+    private String clientContactInformation; // optional contact information of the client booking for the time slot (incl. within the Request body)
 }
