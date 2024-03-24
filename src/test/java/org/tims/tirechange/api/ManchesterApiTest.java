50027b7b3858145cbf9f405478d0f270cb3f7c99
5OO <git.hub@myrk.eu>
2024-03-24 22:24:00 +0200

    Update ManchesterApiTest, introduce new TestCase
    Additional import statements for HttpEntity, HttpMethod, and test assertions were added to the ManchesterApiTest class. The update also includes the refactoring of existing mock setups and addition of a new TestCase, `testBookTimeSlot_Successful`, to validate the bookTimeSlot API's success scenario.

--- src/test/java/org/tims/tirechange/api/ManchesterApiTest.java
+++ src/test/java/org/tims/tirechange/api/ManchesterApiTest.java
@@ -2,6 +2,8 @@ package org.tims.tirechange.api;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +19,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -60,7 +65,7 @@ class ManchesterApiTest {
         LocalDateTime expectedDateTime2 = LocalDateTime.parse("2024-03-20T11:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 
         // Setup mock behavior for restTemplate
-        when(mockRestTemplate.getForEntity(Mockito.anyString(), Mockito.eq(String.class))).thenReturn(new ResponseEntity<>(jsonResponse, HttpStatus.OK));
+        when(mockRestTemplate.getForEntity(Mockito.anyString(), eq(String.class))).thenReturn(new ResponseEntity<>(jsonResponse, HttpStatus.OK));
 
         // Instantiate the ManchesterApi with mocked dependencies
         ManchesterApi manchesterApi = new ManchesterApi(mockRestTemplate, mockConfigLoader);
@@ -116,7 +121,7 @@ class ManchesterApiTest {
         LocalDateTime expectedDateTime2 = LocalDateTime.parse("2024-03-20T11:00:00", DateTimeFormatter.ISO_LOCAL_DATE_TIME);
 
         // Setup mock behavior for restTemplate
-        when(mockRestTemplate.getForEntity(Mockito.anyString(), Mockito.eq(String.class))).thenReturn(new ResponseEntity<>(jsonResponse, HttpStatus.OK));
+        when(mockRestTemplate.getForEntity(Mockito.anyString(), eq(String.class))).thenReturn(new ResponseEntity<>(jsonResponse, HttpStatus.OK));
 
         // Instantiate the ManchesterApi with mocked dependencies
         ManchesterApi manchesterApi = new ManchesterApi(mockRestTemplate, mockConfigLoader);
@@ -128,5 +133,58 @@ class ManchesterApiTest {
         assertThat(result).isEmpty();
 
     }
+
+    @Test
+    void testBookTimeSlot_Successful() throws IOException {
+
+        // Mock dependencies
+        TireShopConfigLoader mockConfigLoader = mock(TireShopConfigLoader.class);
+        RestTemplate mockRestTemplate = mock(RestTemplate.class);
+
+        // Setup mock behavior for configLoader
+        TireShopConfig testConfig = new TireShopConfig();
+        testConfig.setName("Manchester");
+        testConfig.setAddress("14 Bury New Rd, Manchester");
+        testConfig.setVehicleTypes(new String[]{"Car", "Truck"});
+
+        // Create a test ApiConfig instance
+        ApiConfig apiConfig = new ApiConfig();
+
+        apiConfig.setEndpoint("http://localhost:9004/api/v2/tire-change-times/");
+        apiConfig.setType("json");
+
+        testConfig.setApi(apiConfig);
+
+        TireShopConfig dummyConfig = new TireShopConfig(); // Create a dummy config for the first entry
+        dummyConfig.setName("Dummy");
+        dummyConfig.setAddress("Dummy Address");
+        dummyConfig.setVehicleTypes(new String[]{"DummyType"});
+        dummyConfig.setApi(new ApiConfig());
+
+        when(mockConfigLoader.loadConfig("src/main/resources/tire_shops.json")).thenReturn(List.of(dummyConfig, testConfig));
+
+        // Define test data for the request and response
+        String universalId = "60";
+        String contactInformation = "test@example.com";
+        String bookingUrl = testConfig.getApi().getEndpoint() + universalId + "/booking";
+
+        String jsonResponse = "{\n" + "  \"id\": 60,\n" + "  \"time\": \"2024-03-20T09:00:00Z\",\n" + "  \"available\": false\n" + "}";
+
+        // Setup mock behavior for restTemplate
+        when(mockRestTemplate.exchange(eq(bookingUrl), eq(HttpMethod.POST), any(HttpEntity.class), eq(String.class))).thenReturn(new ResponseEntity<>(jsonResponse, HttpStatus.OK));
+
+        // Instantiate ManchesterApi with mocked dependencies
+        ManchesterApi manchesterApi = new ManchesterApi(mockRestTemplate, mockConfigLoader);
+
+        // Execute the test method
+        ManchesterTireChangeTime result = manchesterApi.bookTimeSlot(universalId, contactInformation);
+
+        // Verify the results
+        assertThat(result).isNotNull();
+        assertThat(result.getId()).isEqualTo(60);
+        assertThat(result.getBookingTime()).isEqualTo(LocalDateTime.parse("2024-03-20T09:00:00", DateTimeFormatter.ISO_DATE_TIME));
+        assertThat(result.isAvailable()).isFalse();
+    }
+
 
 }
